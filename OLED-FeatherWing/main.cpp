5f4778af7256507b170063652d4b0552de9bf140
Jongsoo Jeong <jsjeong@coxlab.kr>
2019-04-16 17:54:07 +0900

    Fixed to print on OLED display instead of Serial2.

--- OLED-FeatherWing/main.cpp
+++ OLED-FeatherWing/main.cpp
@@ -3,7 +3,6 @@
 #include <dev/Adafruit_SSD1306.hpp>
 
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
-// Adafruit_SSD1306 display(-1, Wire, 0x3C);
 
 Timer timerHello;
 
@@ -15,33 +14,46 @@ void setup() {
   Serial2.begin(115200);
   Serial2.printf("\n*** [Feather-M0] OLED FeatherWing Test ***\n");
 
-  timerHello.onFired(taskHello, NULL);
+  timerHello.onFired(taskHello, nullptr);
   timerHello.startPeriodic(500);
 
   pinMode(D13, OUTPUT);
   digitalWrite(D13, HIGH);
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
-
-  // display.begin(SSD1306_SWITCHCAPVCC);
-  // display.setTextColor(WHITE);
-  // display.setTextSize(2);
-  // display.clearDisplay();
-  // display.print("* Feather-M0 *");
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("*** [Feather-M0] ***");
+  display.println("OLED FeatherWing Test");
+  display.println();
+  display.println("Press any button!");
   display.display();
 
   pinMode(D5, INPUT_PULLUP);
   attachInterrupt(D5, []() {
-    Serial2.printf("- 'C' is pressed!\n");
+    display.fillRect(0, 24, 128, 8, BLACK);
+    display.setCursor(0, 24);
+    display.print("'C' is pressed!");
+    display.display();
   }, FALLING);
 
   pinMode(D6, INPUT_PULLUP);
   attachInterrupt(D6, []() {
-    Serial2.printf("- 'B' is pressed!\n");
+    display.fillRect(0, 24, 128, 8, BLACK);
+    display.setCursor(0, 24);
+    display.print("'B' is pressed!");
+    display.display();
   }, FALLING);
 
   pinMode(D9, INPUT_PULLUP);
   attachInterrupt(D9, []() {
-    Serial2.printf("- 'A' is pressed!\n");
+    display.fillRect(0, 24, 128, 8, BLACK);
+    display.setCursor(0, 24);
+    display.print("'A' is pressed!");
+    display.display();
   }, FALLING);
 }
