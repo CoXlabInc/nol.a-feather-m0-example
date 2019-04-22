1bf5829492dd6ea967b47283e9b1f449e9bf24e2
Jongsoo Jeong <jsjeong@coxlab.kr>
2019-04-23 08:53:25 +0900

    Added sending to the LoRaWAN.

--- UltimateGPS+OLED/main.cpp
+++ UltimateGPS+OLED/main.cpp
@@ -6,8 +6,65 @@
 #include <nmea/gpgsv.h>
 #include <dev/Adafruit_SSD1306.hpp>
 
+#define SEND_TO_LORAWAN
+
 UltimateGps gps(A5, Serial2);
 Adafruit_SSD1306 display(128, 32, &Wire);
+
+#ifdef SEND_TO_LORAWAN
+#include <LoRaMacKR920.hpp>
+
+SystemFeatherM0::RFM9x RFM95(A1, D10, D11);
+LoRaMacKR920 LoRaWAN(RFM95, 3);
+
+static uint8_t devEui[] = "\x98\x76\xB6\x00\x00\x10\xED\x7D";
+static const uint8_t appEui[] = "\x00\x00\x00\x00\x00\x00\x00\x00";
+static uint8_t appKey[] = "\x94\x62\xef\xdb\x8c\x62\x01\xe7\x52\x09\x8f\x17\x3c\x2a\x4b\x1c";
+
+Timer timerSend;
+
+static void taskPeriodicSend(void *) {
+  LoRaMacFrame *f = new LoRaMacFrame(255);
+  if (!f) {
+    printf("* Out of memory\n");
+    return;
+  }
+
+  f->port = 1;
+  f->type = LoRaMacFrame::CONFIRMED;
+  f->len = sprintf(
+    (char *) f->buf,
+    "\"Now\":%lu",
+    (uint32_t) System.getDateTime()
+  );
+
+  /* Uncomment below line to specify frequency. */
+  // f->freq = 922500000;
+
+  /* Uncomment below lines to specify parameters manually. */
+  // LoRaWAN.useADR = false;
+  // f->modulation = Radio::MOD_LORA;
+  // f->meta.LoRa.bw = Radio::BW_125kHz;
+  // f->meta.LoRa.sf = Radio::SF7;
+  // f->power = 1; /* Index 1 => MaxEIRP - 2 dBm */
+
+  /* Uncomment below line to specify number of trials. */
+  // f->numTrials = 1;
+
+  error_t err = LoRaWAN.send(f);
+  Serial0.printf("* Sending periodic report (%s (%u byte)): %d\n", f->buf, f->len, err);
+  if (err != ERROR_SUCCESS) {
+    delete f;
+    timerSend.startOneShot(10000);
+    return;
+  }
+
+  err = LoRaWAN.requestDeviceTime();
+  printf("* Request DeviceTime: %d\n", err);
+
+  digitalWrite(D13, HIGH);
+}
+#endif //SEND_TO_LORAWAN
 
 void setup() {
   Serial0.begin(115200);
@@ -43,8 +100,8 @@ void setup() {
           nmea_gpgga_s *gga = (nmea_gpgga_s *) parsed;
           Serial0.printf("  Position fix indicator:%u\n", gga->position_fix);
           Serial0.printf("  Time: %02u:%02u:%02u\n", gga->time.tm_hour, gga->time.tm_min, gga->time.tm_sec);
-          Serial0.printf("  Longitude: %d degee %lf %c\n", gga->longitude.degrees, gga->longitude.minutes, gga->longitude.cardinal);
-          Serial0.printf("  Latitude: %d degee %lf %c\n", gga->latitude.degrees, gga->latitude.minutes, gga->latitude.cardinal);
+          Serial0.printf("  Longitude: %d deg %lf %c\n", gga->longitude.degrees, gga->longitude.minutes, gga->longitude.cardinal);
+          Serial0.printf("  Latitude: %d deg %lf %c\n", gga->latitude.degrees, gga->latitude.minutes, gga->latitude.cardinal);
           Serial0.printf("  Altitude: %d %c\n", gga->altitude, gga->altitude_unit);
           Serial0.printf("  Number of satellites: %d\n", gga->n_satellites);
 
@@ -150,8 +207,8 @@ void setup() {
             rmc->time.tm_year + 1900, rmc->time.tm_mon + 1, rmc->time.tm_mday,
             rmc->time.tm_hour, rmc->time.tm_min, rmc->time.tm_sec
           );
-          Serial0.printf("  Longitude: %d degree %lf %c\n", rmc->longitude.degrees, rmc->longitude.minutes, rmc->longitude.cardinal);
-          Serial0.printf("  Latitude: %d degree %lf %c\n", rmc->latitude.degrees, rmc->latitude.minutes, rmc->latitude.cardinal);
+          Serial0.printf("  Longitude: %d deg %lf %c\n", rmc->longitude.degrees, rmc->longitude.minutes, rmc->longitude.cardinal);
+          Serial0.printf("  Latitude: %d deg %lf %c\n", rmc->latitude.degrees, rmc->latitude.minutes, rmc->latitude.cardinal);
           Serial0.printf("  Speed: %lf\n", rmc->speed);
         } else {
           Serial0.printf("  Parsing RMC error (%p)\n", parsed);
@@ -187,4 +244,136 @@ void setup() {
 
   pinMode(D13, OUTPUT);
   digitalWrite(D13, LOW);
+
+#ifdef SEND_TO_LORAWAN
+  System.setTimeDiff(9 * 60);  // KST
+
+  LoRaWAN.begin([]() -> uint8_t {
+    return 0; // means that the device is connected to an external power source.
+  });
+  LoRaWAN.onSendDone([](LoRaMac &lw, LoRaMacFrame *frame) {
+    digitalWrite(D13, LOW);
+    Serial0.printf(
+      "* Send done(%d): destined for port:%u, fCnt:0x%lX, Freq:%lu Hz, "
+      "Power:%d dBm, # of Tx:%u, ",
+      frame->result,
+      frame->port,
+      frame->fCnt,
+      frame->freq,
+      frame->power,
+      frame->numTrials
+    );
+
+    if (frame->modulation == Radio::MOD_LORA) {
+      const char *strBW[] = {
+        "Unknown", "125kHz", "250kHz", "500kHz", "Unexpected value"
+      };
+      if (frame->meta.LoRa.bw > 3) {
+        frame->meta.LoRa.bw = (Radio::LoRaBW_t) 4;
+      }
+      Serial0.printf(
+        "LoRa, SF:%u, BW:%s, ", frame->meta.LoRa.sf, strBW[frame->meta.LoRa.bw]
+      );
+    } else if (frame->modulation == Radio::MOD_FSK) {
+      Serial0.printf("FSK, ");
+    } else {
+      Serial0.printf("Unkndown modulation, ");
+    }
+    if (frame->type == LoRaMacFrame::UNCONFIRMED) {
+      Serial0.printf("UNCONFIRMED");
+    } else if (frame->type == LoRaMacFrame::CONFIRMED) {
+      Serial0.printf("CONFIRMED");
+    } else if (frame->type == LoRaMacFrame::MULTICAST) {
+      Serial0.printf("MULTICAST (error)");
+    } else if (frame->type == LoRaMacFrame::PROPRIETARY) {
+      Serial0.printf("PROPRIETARY");
+    } else {
+      Serial0.printf("unknown type");
+    }
+    Serial0.printf(" frame\n");
+
+    for (uint8_t t = 0; t < frame->numTrials; t++) {
+      const char *strTxResult[] = {
+        "not started",
+        "success",
+        "no ack",
+        "air busy",
+        "Tx timeout",
+      };
+      Serial0.printf("- [%u] %s\n", t, strTxResult[min(frame->txResult[t], 4)]);
+    }
+    delete frame;
+    timerSend.startOneShot(10000);
+  });
+  LoRaWAN.onJoin([](
+    LoRaMac &lw,
+    bool joined,
+    const uint8_t *joinedDevEui,
+    const uint8_t *joinedAppEui,
+    const uint8_t *joinedAppKey,
+    const uint8_t *joinedNwkSKey,
+    const uint8_t *joinedAppSKey,
+    uint32_t joinedDevAddr,
+    const RadioPacket &,
+    uint32_t airTime
+  ) {
+    Serial0.printf("* Tx time of JoinRequest: %lu usec.\n", airTime);
+
+    if (joined) {
+      Serial0.println("* Joining done!");
+      timerSend.startOneShot(10000);
+    } else {
+      Serial0.println("* Joining failed. Retry to join.");
+      lw.beginJoining(devEui, appEui, appKey);
+    }
+  });
+  LoRaWAN.onDeviceTimeAnswered(
+    [](
+      LoRaMac &lw,
+      bool success,
+      uint32_t tSeconds,
+      uint8_t tFracSeconds
+    ) {
+      if (success) {
+        struct tm tLocal, tUtc;
+        System.getDateTime(tLocal);
+        System.getUTC(tUtc);
+        printf(
+          "* LoRaWAN DeviceTime answered: (%lu + %u/256) GPS epoch.\n"
+          "- Adjusted local time: %u-%u-%u %02u:%02u:%02u\n"
+          "- Adjusted UTC time: %u-%u-%u %02u:%02u:%02u\n",
+          tSeconds, tFracSeconds,
+          tLocal.tm_year + 1900, tLocal.tm_mon + 1, tLocal.tm_mday,
+          tLocal.tm_hour, tLocal.tm_min, tLocal.tm_sec,
+          tUtc.tm_year + 1900, tUtc.tm_mon + 1, tUtc.tm_mday,
+          tUtc.tm_hour, tUtc.tm_min, tUtc.tm_sec
+        );
+      } else {
+        printf("* LoRaWAN DeviceTime not answered\n");
+      }
+    },
+    &System
+  );
+
+  LoRaWAN.setPublicNetwork(false);
+
+  Serial0.printf(
+    "* DevEUI: %02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X\n",
+    devEui[0], devEui[1], devEui[2], devEui[3],
+    devEui[4], devEui[5], devEui[6], devEui[7]
+  );
+
+  Serial0.printf(
+    "* AppKey: %02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X\n",
+    appKey[0], appKey[1], appKey[2], appKey[3],
+    appKey[4], appKey[5], appKey[6], appKey[7],
+    appKey[8], appKey[9], appKey[10], appKey[11],
+    appKey[12], appKey[13], appKey[14], appKey[15]
+  );
+
+  Serial0.println("* Let's start join!");
+  LoRaWAN.beginJoining(devEui, appEui, appKey);
+
+  timerSend.onFired(taskPeriodicSend, nullptr);
+#endif //SEND_TO_LORAWAN
 }
